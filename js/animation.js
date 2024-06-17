a2fac226194e9da27efa0a421cfc2834fed4073a
DinaSharipova <sharipova0308@gmail.com>
2024-06-17 14:48:39 +0500

    add edits

--- js/animation.js
+++ js/animation.js
@@ -1,31 +1,23 @@
 function animation() {
     gsap.registerPlugin(ScrollTrigger);
-  
-    gsap.from('.promo__title span', {
+
+    if (document.documentElement.clientWidth > 767) {
+        gsap.from('.promo__title span', {
         opacity: 0,
         stagger: 0.3,
     });
+    }
+  
+    
 
-    gsap.to('.anim-scale', {
-        
-        scale: 1.2,
-        delay: 1,
-    })
-
-    gsap.to('.anim-scale', {
-        
-        scale: 1,
-        delay: 1.5,
-    })
-
-    gsap.from('.work__item', {
+      gsap.from('.work__item', {
       opacity: 0,
       yPercent: 30,
         
       stagger: 0.4,
       scrollTrigger: {
         trigger: '.work',
-        start: 'top 35%', 
+        start: 'top 45%', 
        
         toggleActions: 'play none none reverse',
       }
@@ -37,7 +29,7 @@ function animation() {
         stagger: 0.4,
         scrollTrigger: {
           trigger: '.wins',
-          start: 'top 35%', 
+          start: 'top 45%', 
          
           toggleActions: 'play none none reverse',
         }
